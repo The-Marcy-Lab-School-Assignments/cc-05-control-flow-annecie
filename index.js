de3f72af35d3b2678e7be6255b2204fb8b11da02
annecie <ajbexcelsior@gmail.com>
2021-10-06 14:46:22 +0000

    add changes

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 //1
-function absoluteValue(){
-  
+function absoluteValue(number){
+    if (number > 0){
+        console.log(number);
+    }else if (number < 0){
+        console.log(number *= -1)
+        
+    }else{
+      return null  
+   }
 }
 
-
+console.log(absoluteValue("3"))
 
 
